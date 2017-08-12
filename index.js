0cac19fc235ac7b2123649c5debff61b2e9861a5
Kaan İnel <kaaninel@gmail.com>
2017-08-12 16:20:55 +0300

    jsonfn for functions

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Locker = require("asynclock")();
-
+const {JSONfn} = require("jsonfn");
 const IdG = function*(id){
   while(true) 
     for(let i = 0; i < Number.MAX_SAFE_INTEGER; i++) 
@@ -11,12 +11,13 @@ module.exports = {
     const cId = IdG(id);
     return function(Command, ...Args){
       const Id = cId.next().value;
-      fn.call(th, {Id, Command, Args});
+      fn.call(th, JSONfn.stringify({Id, Command, Args}));
       return Locker.Lock(Id);
     }
   },
   Receive: function(th, fn, Commands){
     return async function(Data){
+      Data = JSONfn.parse(Data);
       if(Data.Command && Data.Command in Commands)
         fn.call(th,{Id: Data.Id, Result: await Commands[Data.Command](...Data.Args)});
       else(Data.Result)
